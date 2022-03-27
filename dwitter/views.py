6db37ea0a6bab4ca6fff8035f5215f18b93f3c75
Alexandre Karpov <alexakarpov@gmail.com>
2022-03-27 01:55:56 -0400

    form_and_redirect

--- dwitter/views.py
+++ dwitter/views.py
@@ -1,16 +1,32 @@
 # dwitter/views.py
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Profile
+from .forms import DweetForm
 
 def dashboard(request):
+  form = DweetForm(request.POST or None)
+  if request.method == "POST":
+    if form.is_valid():
+      print("form is valid")
+      dweet = form.save(commit=False)
+      dweet.user = request.user
+      dweet.save()
+      return redirect("dwitter:dashboard")
+    else:
+      print("form not valid")
+
   follows = request.user.profile.follows.all()
   sorted_dweets = []
   for f in follows:
     sorted_dweets.extend(list(f.user.dweets.all()))
-
   sorted_dweets.sort(key=lambda d: d.created_at, reverse=True)
-  return render(request, "dwitter/dashboard.html", {'dweets': sorted_dweets})
+  return render(request,
+                "dwitter/dashboard.html",
+                {
+                 'dweets': sorted_dweets,
+                 'form': form
+                })
 
 def profiles(request):
   profiles = Profile.objects.exclude(user=request.user)
